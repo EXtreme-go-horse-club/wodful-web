2e3a36a81d8b05ad121bb85824a94e2c61376d34
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2022-09-24 11:39:40 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Box bg="red.900" fontSize="md">
+        Welcome
+      </Box>
+    </div>
+  );
+}
+
+export default App;
