7e35636523b9253cd0c74a7c64f3b9247ca35fbe
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2022-11-04 00:12:40 -0300

    WWEB#26 change search flow

--- src/pages/private/Participants/index.tsx
+++ src/pages/private/Participants/index.tsx
@@ -1,31 +1,18 @@
 import { Loader } from '@/components/Loader';
 import { ParticipantProvider } from '@/contexts/participant';
-import {
-  Box,
-  Button,
-  HStack,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Text,
-} from '@chakra-ui/react';
-import { ChangeEvent, lazy, Suspense, useEffect, useState } from 'react';
-import { Filter, Search, X } from 'react-feather';
+import { Box, Button, HStack, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
+import { ChangeEvent, lazy, Suspense, useState } from 'react';
+import { Filter, Search } from 'react-feather';
 
 const ListParticipants = lazy(() => import('./components/list'));
 
 const Participants = () => {
   const [participantName, setParticipantName] = useState<string>('');
-  const [inputValue, setInputValue] = useState<string | null>(null);
 
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    return setInputValue(event.target.value);
+    const name = event.target.value.length;
+    return name >= 3 ? setParticipantName(event.target.value) : setParticipantName('');
   };
-
-  useEffect(() => {
-    setParticipantName('');
-  }, [inputValue]);
 
   return (
     <Suspense fallback={<Loader title='Carregando ...' />}>
@@ -46,20 +33,13 @@ const Participants = () => {
                   w='100%'
                   minW='320px'
                   placeholder='Buscar participante'
-                  value={inputValue || ''}
                 />
-                {inputValue && (
-                  <InputRightElement>
-                    <X cursor='pointer' size={20} color='gray' onClick={() => setInputValue('')} />
-                  </InputRightElement>
-                )}
               </InputGroup>
               <Button
                 minW='84px'
                 leftIcon={<Filter size={14} />}
                 variant='outline'
                 colorScheme='gray'
-                onClick={() => setParticipantName(inputValue as string)}
               >
                 Filtrar
               </Button>
