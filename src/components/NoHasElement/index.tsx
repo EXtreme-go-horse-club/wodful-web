f587888f2a842f2a9a8f89cb66f84ace231b00ef
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2022-11-07 11:10:03 -0300

    WWEB#30 creating NoHasElement component

--- src/components/NoHasElement/index.tsx
+++ src/components/NoHasElement/index.tsx
@@ -1,20 +1,37 @@
-import { Box, Button, Text } from '@chakra-ui/react';
+import { Box, Button, Link, Text } from '@chakra-ui/react';
 import { FolderPlus } from 'react-feather';
+import { Link as RouterLink } from 'react-router-dom';
 
 interface NoHasElementProps {
   text: string;
-  contentButton: string;
-  onClose: () => void;
+  linkTo?: string;
+  textLinkTo?: string;
+  contentButton?: string;
+  onClose?: () => void;
 }
 
-export const NoHasElement = ({ contentButton, onClose, text }: NoHasElementProps) => {
+export const NoHasElement = ({
+  contentButton,
+  onClose,
+  text,
+  linkTo,
+  textLinkTo,
+}: NoHasElementProps) => {
   return (
     <Box display='flex' flexDirection='column' alignItems='center' gap='8px' mt='20%'>
       <FolderPlus size={50} opacity='80%' />
       <Text>{text}</Text>
-      <Button width='100%' colorScheme='teal' onClick={onClose}>
-        {contentButton}
-      </Button>
+      {!linkTo && (
+        <Button width='100%' colorScheme='teal' onClick={onClose}>
+          {contentButton}
+        </Button>
+      )}
+
+      {linkTo && (
+        <Link as={RouterLink} color='teal.500' to={linkTo as string}>
+          {textLinkTo}
+        </Link>
+      )}
     </Box>
   );
 };
