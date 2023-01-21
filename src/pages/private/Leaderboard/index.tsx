dd28ae8df21646b3614d5b11e65032020305a782
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2023-01-21 12:30:22 -0300

    feat: adding hasElement into leaderboard and schedule

--- src/pages/private/Leaderboard/index.tsx
+++ src/pages/private/Leaderboard/index.tsx
@@ -1,5 +1,6 @@
-import { lazy, Suspense, useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useMemo, useState } from 'react';
 
+import { EmptyList } from '@/components/EmptyList';
 import { Loader } from '@/components/Loader';
 import { CategoryProvider } from '@/contexts/category';
 import { LeaderboardProvider } from '@/contexts/leaderboard';
@@ -31,7 +32,10 @@ const Leaderboard = () => {
   const { List: CategoryList, categories } = useCategoryData();
   const [selectedCategory, setSelectedCategory] = useState<string>('Sem categoria');
   const [categoryId, setCategoryId] = useState<string>('');
+
   const { ListPaginated } = useLeaderboardData();
+
+  const hasElements: boolean = useMemo(() => categories?.length !== 0, [categories]);
 
   useEffect(() => {
     if (id) CategoryList(id);
@@ -48,51 +52,64 @@ const Leaderboard = () => {
         alignItems='center'
         p={6}
       >
-        <HStack as='section' role='textbox' w='100%' justifyContent='space-between'>
-          <Flex as='article' role='textbox' direction='column' gap='0.75rem'>
-            <Text fontSize='2xl' as='b' role='heading'>
-              Leaderboard
-            </Text>
-            <Text
-              as='b'
-              role='textbox'
-              fontSize='0.75rem'
-              color='gray.500'
-              border='1px'
-              borderColor='gray.500'
-              borderRadius='4px'
-              padding='2px 8px'
-              textTransform='capitalize'
-            >
-              Categoria: {selectedCategory}
-            </Text>
-          </Flex>
-          <Flex as='article' gap='1rem'>
-            <Select
-              as='select'
-              id='category'
-              placeholder='Selecione a categoria'
-              onChange={(event) => {
-                if (event.target.value) {
-                  ListPaginated(String(id), event.target.value);
-                  setCategoryId(event.target.value);
-                  setSelectedCategory(
-                    categories.find((selected) => selected.id === event.target.value)!.name,
-                  );
-                }
-              }}
-            >
-              {categories?.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-            </Select>
-          </Flex>
-        </HStack>
-        <Box as='section' w='100%' marginTop={6}>
-          <ListLeaderboard category={categoryId as string} champ={id as string} />
-        </Box>
+        {hasElements && (
+          <HStack as='section' role='textbox' w='100%' justifyContent='space-between'>
+            <Flex as='article' role='textbox' direction='column' gap='0.75rem'>
+              <Text fontSize='2xl' as='b' role='heading'>
+                Leaderboard
+              </Text>
+              <Text
+                as='b'
+                role='textbox'
+                fontSize='0.75rem'
+                color='gray.500'
+                border='1px'
+                borderColor='gray.500'
+                borderRadius='4px'
+                padding='2px 8px'
+                textTransform='capitalize'
+              >
+                Categoria: {selectedCategory}
+              </Text>
+            </Flex>
+            <Flex as='article' gap='1rem'>
+              <Select
+                as='select'
+                id='category'
+                placeholder='Selecione a categoria'
+                onChange={(event) => {
+                  if (event.target.value) {
+                    ListPaginated(String(id), event.target.value);
+                    setCategoryId(event.target.value);
+                    setSelectedCategory(
+                      categories.find((selected) => selected.id === event.target.value)!.name,
+                    );
+                  }
+                }}
+              >
+                {categories?.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </Select>
+            </Flex>
+          </HStack>
+        )}
+
+        {hasElements && (
+          <Box as='section' w='100%' marginTop={6}>
+            <ListLeaderboard category={categoryId as string} champ={id as string} />
+          </Box>
+        )}
+
+        {!hasElements && (
+          <EmptyList
+            text='Você não possui um leaderboard ainda!'
+            linkTo={`/championships/${id}/categories`}
+            textLinkTo='Começe seu campeonato aqui.'
+          />
+        )}
       </Box>
     </Suspense>
   );
