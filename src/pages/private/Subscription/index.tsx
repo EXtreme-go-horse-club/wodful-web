7fab57234b2ffe6cbe2af4a6b3ed578ed8d7ca48
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2023-03-04 15:34:36 -0300

    Removendo use memo

--- src/pages/private/Subscription/index.tsx
+++ src/pages/private/Subscription/index.tsx
@@ -7,7 +7,7 @@ import { TicketProvider } from '@/contexts/ticket';
 import useCategoryData from '@/hooks/useCategoryData';
 import useSubscriptionData from '@/hooks/useSubscriptionData';
 import { Box, Button, Flex, HStack, Select, Text, useDisclosure } from '@chakra-ui/react';
-import { ChangeEvent, lazy, Suspense, useCallback, useEffect, useMemo, useState } from 'react';
+import { ChangeEvent, Suspense, lazy, useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FormSubscriptionParticipants from './components/formParticipants';
 import FormSubscription from './components/formSubscription';
@@ -43,18 +43,16 @@ const Subscription = () => {
     setCurrentStep(step);
   }
 
-  const resetCategoryData= () =>{
-    setSelectedCategory("Todos");
+  const resetCategoryData = () => {
+    setSelectedCategory('Todos');
     setCategoryId('');
-  }
+  };
 
   const openCreate = () => {
     resetCategoryData();
     ListPaginated(id as string);
     onOpen();
   };
-
-  
 
   const handleChangeCategory = useCallback(
     (event: ChangeEvent<HTMLSelectElement>) => {
@@ -76,54 +74,52 @@ const Subscription = () => {
     }
   }, [CategoryList, id]);
 
-
   return (
     <Suspense fallback={<Loader title='Carregando ...' />}>
       <Box w='100%' display='flex' flexDirection='column' alignItems='center' p={6}>
-          <>
-            <HStack w='100%' justifyContent='space-between'>
-              <Flex as='article' role='textbox' direction='column' gap='0.75rem'>
-                <Text fontSize='2xl' as='b' role='heading'>
-                  Inscrições
-                </Text>
-                <Text
-                  as='b'
-                  role='textbox'
-                  fontSize='0.75rem'
-                  color='gray.500'
-                  border='1px'
-                  borderColor='gray.500'
-                  borderRadius='4px'
-                  padding='2px 8px'
-                  textTransform='capitalize'
-                >
-                  Categoria: {selectedCategory}
-                </Text>
-              </Flex>
-              <Flex as='article' gap='1rem'>
-                <Select
-                  as='select'
-                  id='category'
-                  value={categoryId}
-                  placeholder='Selecionar categoria'
-                  onChange={(event) => handleChangeCategory(event)}
-                >
-                  {categories?.map((category) => (
-                    <option key={category.id} value={category.id}>
-                      {category.name}
-                    </option>
-                  ))}
-                </Select>
-                <Button minW='170px' colorScheme='teal' size='md' onClick={openCreate}>
-                  Adicionar inscrição
-                </Button>
-              </Flex>
-            </HStack>
-            <Box w='100%' marginTop={6}>
-              <ListSubscription id={id as string} categoryId={categoryId} />
-            </Box>
-          </>
-
+        <>
+          <HStack w='100%' justifyContent='space-between'>
+            <Flex as='article' role='textbox' direction='column' gap='0.75rem'>
+              <Text fontSize='2xl' as='b' role='heading'>
+                Inscrições
+              </Text>
+              <Text
+                as='b'
+                role='textbox'
+                fontSize='0.75rem'
+                color='gray.500'
+                border='1px'
+                borderColor='gray.500'
+                borderRadius='4px'
+                padding='2px 8px'
+                textTransform='capitalize'
+              >
+                Categoria: {selectedCategory}
+              </Text>
+            </Flex>
+            <Flex as='article' gap='1rem'>
+              <Select
+                as='select'
+                id='category'
+                value={categoryId}
+                placeholder='Selecionar categoria'
+                onChange={(event) => handleChangeCategory(event)}
+              >
+                {categories?.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </Select>
+              <Button minW='170px' colorScheme='teal' size='md' onClick={openCreate}>
+                Adicionar inscrição
+              </Button>
+            </Flex>
+          </HStack>
+          <Box w='100%' marginTop={6}>
+            <ListSubscription id={id as string} categoryId={categoryId} />
+          </Box>
+        </>
 
         <ComponentModal
           modalHeader='Adicionar inscrição'
